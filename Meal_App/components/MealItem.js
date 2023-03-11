00b92fe7f9452dedd06068eb664b368699d5a554
MerveKpln <hamuss001@gmail.com>
2023-03-11 18:21:55 +0400

    v4

---
+++ Meal_App/components/MealItem.js
@@ -0,0 +1,12 @@
+import { Text, View, Pressable } from "react-native";
+
+function MealItem({ title }) {
+  return (
+    <View>
+      <Pressable></Pressable>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+export default MealItem;
