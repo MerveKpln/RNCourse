e0afbaa7217aad1b5a2ee5bc9642b808a844c986
MerveKpln <hamuss001@gmail.com>
2023-03-15 10:39:33 +0400

    v9

--- Expense_Tracker_App/screens/ManageExpense.js
+++ Expense_Tracker_App/screens/ManageExpense.js
@@ -1,6 +1,47 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
-function ManageExpense() {
-  return <Text>Manage expenses</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteConatiner}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteConatiner: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
